811ab0ee40a9aa1d4f7ae33a4b75e92f5092ea37
SpudiQ <yura_anisimov@list.ru>
2026-03-26 21:09:30 +0300

    feat[frontend]: HMR for docker

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+	compatibilityDate: "2025-07-15",
+	devtools: { enabled: true },
+	vite: {
+		server: {
+			watch: {
+				usePolling: true, // NOTE: для Docker на Windows/Mac
+				interval: 1000,
+			},
+			hmr: {
+				protocol: "ws",
+				host: "localhost",
+				port: 24678,
+			},
+		},
+	},
+});
